e6e695b31b1be5f2d3ed6ad83438feb386dfed12
dmvoloshin <ditrix2006@gmail.com>
2025-05-02 14:54:35 +0300

    gitignore changes

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,13 +5,18 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import HtmlSupport from '@ckeditor/ckeditor5-html-support/src/htmlsupport';
+
 class ClassicEditor extends ClassicEditorBase {}
 
 ClassicEditor.builtinPlugins = [
   Essentials,
   Paragraph,
   Bold,
-  Italic
+  Italic,
+  PasteFromOffice,
+  HtmlSupport
 ];
 
 ClassicEditor.defaultConfig = {
